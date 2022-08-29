d7dc91371a6f321137b1b7b8bb059c92062b32ca
mariasza <maria_araujo_souza@hotmail.com>
2022-08-29 16:58:21 -0400

    chore: dto's

--- src/app/equipment/dto/create-equipment.dto.ts
+++ src/app/equipment/dto/create-equipment.dto.ts
@@ -1,1 +1,23 @@
-export class CreateEquipmentDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateEquipmentDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  brand: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  amount: number;
+
+  @ApiPropertyOptional()
+  @IsString()
+  observation: string;
+}
