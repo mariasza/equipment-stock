0fc69e74ebc62793e1404ef064572a060b87344f
mariasza <maria_araujo_souza@hotmail.com>
2022-08-29 17:54:35 -0400

    chore: config heroku database

--- src/database/config.js
+++ src/database/config.js
@@ -1,12 +1,15 @@
 module.exports = {
   development: {
+    username: 'vfqqsobjvgdssc',
+    password:
+      '221195999280e0796d4fb0dd9b92b0e03f7a76b760720448cba1d3ba42a93394',
+    database: 'd5l17cuangv5bv',
+    port: 5432,
+    host: 'ec2-44-205-63-142.compute-1.amazonaws.com',
+    uri: 'postgres://vfqqsobjvgdssc:221195999280e0796d4fb0dd9b92b0e03f7a76b760720448cba1d3ba42a93394@ec2-44-205-63-142.compute-1.amazonaws.com:5432/d5l17cuangv5bv',
+    dialect: 'postgres',
     username: 'pguser',
-    password: 'pgpassword',
-    database: 'pgsql',
-    host: 'localhost',
     port: 5432,
     ssl: { rejectUnauthorized: false },
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres',
   },
 };
--- src/database/sequelizeOptions.ts
+++ src/database/sequelizeOptions.ts
@@ -2,10 +2,12 @@ import { SequelizeModuleOptions } from '@nestjs/sequelize';
 import { Equipments } from './models/equipment';
 
 export const configSequelize: SequelizeModuleOptions = {
-  username: 'pguser',
-  password: 'pgpassword',
-  database: 'pgsql',
+  username: 'vfqqsobjvgdssc',
+  password: '221195999280e0796d4fb0dd9b92b0e03f7a76b760720448cba1d3ba42a93394',
+  database: 'd5l17cuangv5bv',
   port: 5432,
+  host: 'ec2-44-205-63-142.compute-1.amazonaws.com',
+  uri: 'postgres://vfqqsobjvgdssc:221195999280e0796d4fb0dd9b92b0e03f7a76b760720448cba1d3ba42a93394@ec2-44-205-63-142.compute-1.amazonaws.com:5432/d5l17cuangv5bv',
   dialect: 'postgres',
   timezone: '-04:00',
   models: [Equipments],
