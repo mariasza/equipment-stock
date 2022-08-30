9024008c2546dc649e9abb9c4404aee213e7504d
mariasza <maria_araujo_souza@hotmail.com>
2022-08-30 19:16:56 -0400

    fix: sequelize

--- src/database/config.js
+++ src/database/config.js
@@ -1,16 +1,10 @@
 module.exports = {
   development: {
-    username: process.env.NF_EQUIPMENT_STOCK_DB_USERNAME,
-    password: process.env.NF_EQUIPMENT_STOCK_DB_PASSWORD,
-    database: process.env.NF_EQUIPMENT_STOCK_DB_DATABASE,
-    port: parseInt(process.env.NF_EQUIPMENT_STOCK_DB_PORT),
+    username: 'user',
+    password: '12345',
+    database: 'mysqldb',
+    host: 'localhost',
+    port: 3306,
     dialect: 'mysql',
-    host: process.env.NF_EQUIPMENT_STOCK_DB_HOST,
-    ssl: true,
-    dialectOptions: {
-      ssl: {
-        ca: fs.readFileSync(process.env.SSL_CA_CERTIFICATES),
-      },
-    },
   },
 };
