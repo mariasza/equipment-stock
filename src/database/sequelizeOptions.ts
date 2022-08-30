b0f379bc888fca9fb8ac2d26ecb70fa3ea7665ec
mariasza <maria_araujo_souza@hotmail.com>
2022-08-30 18:13:29 -0400

    fix: sequelize

--- src/database/sequelizeOptions.ts
+++ src/database/sequelizeOptions.ts
@@ -2,7 +2,7 @@ import { SequelizeModuleOptions } from '@nestjs/sequelize';
 import { Equipments } from './models/equipment';
 
 export const configSequelize: SequelizeModuleOptions = {
-  username: 'user',
+  name: 'user',
   password: '12345',
   database: 'mysqldb',
   port: 3306,
